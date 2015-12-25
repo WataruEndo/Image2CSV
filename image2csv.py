0b682d28dc5b6c5302f83310297b52a8f12477cc
Wataru <wtr.endo@gmail.com>
2015-12-25 17:00:10 +0000

    ver 1.0.

--- image2csv.py
+++ image2csv.py
@@ -6,18 +6,21 @@ import random
 import string
 import glob
 
-class Image2csv:
-    def __init__(self, dir, clip_size, dim, colortype, src_image_xy):
+class Image2CsvTraindata:
+    def __init__(self, dir, clip_size, dim, src_image_xy):
         self.dir = dir
         self.clip_size = clip_size
         self.dim = dim
-        self.colortype = colortype # not implemented.
         self.src_image_xy = src_image_xy
         self.scan_xy = (src_image_xy[0] - (clip_size[0] - 1),\
                         src_image_xy[1] - (clip_size[1] - 1))
         self.scan_size = self.scan_xy[0] * self.scan_xy[1]
 
     def openimages(self, path):
+        """"openimages" open images and get category(label).
+        Images must be "[Category-number]_xxx.fileformat."
+        This function regards prefix as a category.
+        """
         open_files = glob.glob(path)
         category = []
         src_image = []
@@ -31,14 +34,15 @@ class Image2csv:
 
     def traindata_create(self, src_image, output_filename,\
                          random_flg, seed=None):
-        """"traindata_create" create training-data for pylearn2 as CSV format.
+        """"traindata_create" create training-data
+            for icml emoption on pylearn2 as CSV format.
         """
 
         if random_flg == 1:
             if seed == None:
                 random.seed(None)
             else:
-                print seed
+                print "random seed fixed : %d" %seed
                 random.seed(seed)
 
             readlist = random.sample(xrange(self.scan_size),\
@@ -46,13 +50,14 @@ class Image2csv:
         else:
             readlist = xrange(self.scan_size)
 
+        #Create csvfile.
         f = open(output_filename, "w")
         index =0
         for base_point in readlist:
             for index in range(len(src_image[0])):
                 tmp = self.clip(src_image[0][index], src_image[1][index],\
                                 base_point, self.clip_size)
-                self.pixel_2_csvline(f, src_image[0][index],tmp)
+                self.image_2_csvline(f, src_image[0][index],tmp)
 
         f.close()
 
@@ -64,8 +69,9 @@ class Image2csv:
                 clipped.append(src_image[read_point])
         return clipped
 
-    def pixel_2_csvline(self, writer, category, image):
-        print category
+    def image_2_csvline(self, writer, category, image):
+        """"image_2_scvline" transfer clipped image to csvline.
+        """
         for index, output in enumerate(image):
             if index == 0:
                 outline = str(category) + "," + str(output[0])
@@ -79,6 +85,6 @@ class Image2csv:
 
 
 if __name__ == "__main__":
-    I2C=Image2csv("./", (8,8), 3, "rgb", (400,400))
-    src = I2C.openimages("[^0-9]*.jpeg")
-    I2C.traindata_create(src, "train.csv", 1, 1)
+    I2CT=Image2CsvTraindata("./", (8,8), 3, (400,400))
+    src = I2CT.openimages("[^0-9]*.jpeg")
+    I2CT.traindata_create(src, "train.csv", 1, 1)
