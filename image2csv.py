59ba4800fd88939fcdda620a5616e993bdd0d570
Wataru <wtr.endo@gmail.com>
2016-01-29 10:29:03 +0000

    tmp.

--- image2csv.py
+++ image2csv.py
@@ -7,7 +7,7 @@ import string
 import glob
 
 class Image2CsvTraindata:
-    def __init__(self, dir, clip_size, dim, src_image_xy):
+    def __init__(self, dir, clip_size, dim, src_image_xy, output_type):
         self.dir = dir
         self.clip_size = clip_size
         self.dim = dim
@@ -15,6 +15,7 @@ class Image2CsvTraindata:
         self.scan_xy = (src_image_xy[0] - (clip_size[0] - 1),\
                         src_image_xy[1] - (clip_size[1] - 1))
         self.scan_size = self.scan_xy[0] * self.scan_xy[1]
+        self.output_type = output_type #icml or libsvm
         self.count=0
 
     def openimages(self, path):
@@ -53,7 +54,9 @@ class Image2CsvTraindata:
 
         #Create csvfile.
         f = open(output_filename, "w")
-        f.write("category,pixels\n")
+        #If icml_train
+        if self.output_type == "icml_train":
+            f.write("category,pixels\n")
 
         for base_point in readlist:
             for index in range(len(src_image[0])):
@@ -77,23 +80,46 @@ class Image2CsvTraindata:
         #self.count=0
         for index, output in enumerate(image):
             if index == 0:
-                outline = str(category) + "," + str(output[0])
+                if self.output_type == "svm":
+                    outline = str(category) + " 1:" + str(output[0])
+                elif self.output_type == "icml_train":
+                    outline = str(category) + "," + str(output[0])
+                else:
+                    outline = str(category) + " " + str(output[0])
+
                 #print self.count
                 for i in range(1, self.dim):
-                    outline = str(outline) + " " + str(output[i])
+                    if self.output_type == "svm":
+                        outline = str(outline) + " " \
+                                + str(i+1) + ":" \
+                                + str(output[i])
+                    else:
+                        outline = str(outline) + " " + str(output[i])
                     self.count = self.count + 1
                     #print self.count
             else:
-                for ii in range(self.dim):
-                    outline = str(outline) + " " + str(output[i])
+                for i in range(self.dim):
+                    if self.output_type == "svm":
+                        outline = str(outline) + " " \
+                          + str(i+1+index*self.dim) + ":" \
+                          + str(output[i])
+                    else:
+                        outline = str(outline) + " " + str(output[i])
                     self.count = self.count + 1
                     #print self.count
 
         outline = str(outline) + "\n"
         writer.write(outline)
 
-
 if __name__ == "__main__":
-    I2CT=Image2CsvTraindata("./", (8,8), 3, (1300,40))
+    #I2CT=Image2CsvTraindata("./", (8,8), 3, (1300,40), "icml_train")
+    I2CT=Image2CsvTraindata("./", (8,8), 3, (1240,40), "icml_test")
+    #I2CT=Image2CsvTraindata("./", (8,8), 3, (1300,40), "svm")
+    #I2CT=Image2CsvTraindata("./", (8,8), 3, (1240,40), "svm")
+    #I2CT=Image2CsvTraindata("./", (1,1), 3, (1240,40), "svm")
     src = I2CT.openimages("[^0-9]*.png")
-    I2CT.traindata_create(src, "train.csv", 1, 1)
+    #I2CT.traindata_create(src, "train.csv", 1, 1)
+    I2CT.traindata_create(src, "test.csv", 1, 1)
+    #I2CT.traindata_create(src, "train.txt", 1, 1)
+    #I2CT.traindata_create(src, "test_svm3.txt", 1, 1)
+    #I2CT.traindata_create(src, "test_svm8.txt", 1, 1)
